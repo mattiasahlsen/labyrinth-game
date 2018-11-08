452ccd6070157d4995efa8ea43dfac8989c45cef
Emil Murman <emilmurman@hotmail.com>
2018-11-08 22:42:31 +0100

    collision detection

--- game/game_state.py
+++ game/game_state.py
@@ -13,11 +13,23 @@ class Game_State:
     def set_vel(self, player_number, direction):
         self.players[player_number].vel = direction
 
+    def legal_move(self, next_pos):
+        x = next_pos[0]
+        y = next_pos[1]
+        # Out of bounds
+        if x < 0 or x >= self.maze.width or y < 0 or y >= self.maze.width:
+            return False
+        # Maze wall
+        if self.maze.maze[y * self.maze.width + x]:
+            return False
+        return True
+
     def tick(self):
         for player in self.players:
-            player.move()
+            if self.legal_move(player.next_pos()):
+                player.move()
             player.vel = (0, 0)
-        
+
     def state_as_json(self):
         data = []
         for player in self.players:
--- game/player.py
+++ game/player.py
@@ -10,6 +10,9 @@ class Player:
         self.x += self.vel[0]
         self.y += self.vel[1]
     
+    def next_pos(self):
+        return (self.x + self.vel[0], self.y + self.vel[1])
+
     def toJSON(self):
         return json.dumps(dict([('player_number', self.player_number), ('x', self.x), ('y', self.y)]))
     
