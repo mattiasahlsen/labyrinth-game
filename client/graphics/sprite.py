ac2569d85a3fbeb7ace23fbcbf28b5bdd71451bf
Emil Murman <emilmurman@hotmail.com>
2018-11-12 13:02:52 +0100

    tons of refactoring, multiple bug fixes, simplification

--- client/graphics/sprite.py
+++ client/graphics/sprite.py
@@ -24,20 +24,31 @@ class Sprite(pygame.sprite.Sprite):
 
         get_image = lambda n: os.path.join(DIR, 'sprites/sprites/elf_f_run_anim_f' + str(n) + '.png')
         self.images = []
+        self.images_right = True
         for n in range(0, 4):
             self.images.append(pygame.image.load(get_image(n)))
         self.image_number = 0
         self.image = self.images[self.image_number]
         self.update_sprite = 1 # when it's 0, update sprite
 
-        self.x_offset = self.image.get_width() / 2
-        self.y_offset = self.image.get_height() * 0.75
+        # Scale sprites
+        size_margin = 1  # make the character slightly bigger - looks nicer
+        scaling_factor = block_size / self.image.get_width()
+        new_dimensions = (  
+            math.floor(block_size * size_margin),
+            math.floor(self.image.get_height() * scaling_factor * size_margin))
+
+        for i in range(0, 4):
+            self.images[i] = pygame.transform.scale(self.images[i], new_dimensions)
+
+        self.x_offset = -2 * scaling_factor * size_margin    # the sprite should be moved about 2 pixels to the right
+        self.y_offset = 12 * scaling_factor * size_margin    # move 12 pixels up
 
         self.radius = self.block_size / 2 # pixels to center
-        self.x = player.x * block_size + self.radius # pixel position
-        self.y = player.y * block_size + self.radius
+        self.x = player.x * block_size
+        self.y = player.y * block_size
         self.rect = pygame.Rect(round(self.x - self.x_offset), round(self.y - self.y_offset),
-                                self.image.get_width(), self.image.get_height())
+                                self.image.get_width(), self.image.get_width())
 
         self.prio = 'x'
 
@@ -48,10 +59,14 @@ class Sprite(pygame.sprite.Sprite):
         self.update_sprite = (self.update_sprite + 1) % SPRITE_UPDATE_INTERVAL
 
         if self.player.local:
+            if self.player.vel[0] < 0 and self.images_right:
+                self.flip_direction()
+            elif self.player.vel[0] > 0 and not self.images_right:
+                self.flip_direction()
+                
             if self.player.illegal_move:
                 self.player.illegal_move = False
                 (self.x, self.y) = self.to_pixels((self.player.x, self.player.y))
-                (self.x, self.y) = (self.x + self.radius, self.y + self.radius)
             else:
                 self.handle_collision()
         else:
@@ -59,8 +74,14 @@ class Sprite(pygame.sprite.Sprite):
             self.x = self.x + (realX - self.x) / FRAMES_PER_TICK
             self.y = self.y + (realY - self.y) / FRAMES_PER_TICK
 
-        self.rect[0], self.rect[1] = (round(self.x - self.x_offset),
-                                      round(self.y - self.y_offset))
+        self.rect[0], self.rect[1] = (math.floor(self.x - self.x_offset),
+                                      math.floor(self.y- self.y_offset))
+
+    def flip_direction(self):
+        for i in range(len(self.images)):
+            self.images[i] = pygame.transform.flip(self.images[i], True, False)
+        self.images_right = not self.images_right
+        self.x_offset = -self.x_offset
 
     def handle_collision(self):
         # New x, y
@@ -68,17 +89,17 @@ class Sprite(pygame.sprite.Sprite):
         new_y = self.y + self.pixels_per_frame * self.player.vel[1]
 
         # Check if new x,y is within the map
-        if new_x - self.radius < 0:
-            new_x = self.radius
-        elif new_x + self.radius > self.res:
-            new_x = self.res - self.radius
-        if new_y - self.radius < 0:
-            new_y = self.radius
-        elif new_y + self.radius > self.res:
-            new_y = self.res - self.radius
+        if new_x < 0:
+            new_x = 0
+        elif new_x >= self.res:
+            new_x = self.res - 1
+        if new_y < 0:
+            new_y = 0
+        elif new_y  > self.res:
+            new_y = self.res - 1
 
         def make_rect(x, y):
-            return pygame.Rect(x - self.radius, y - self.radius,
+            return pygame.Rect(x, y,
                                 self.block_size, self.block_size)
 
         bounding_rect = make_rect(new_x, new_y)
@@ -86,15 +107,15 @@ class Sprite(pygame.sprite.Sprite):
         wall = bounding_rect.collidelist(self.walls)
         if not wall == -1:
             if make_rect(self.x, new_y).collidelist(self.walls) == -1:
-                new_x = self.x
+                new_x = self.round_pixel(self.x + self.radius)
             elif make_rect(new_x, self.y).collidelist(self.walls) == -1:
-                new_y = self.y
+                new_y = self.round_pixel(self.y + self.radius)
             else:
                 new_x, new_y = self.x, self.y
 
         self.x, self.y = new_x, new_y
 
-        new_pos = self.to_coords((self.x, self.y))
+        new_pos = self.to_coords((self.x + self.radius, self.y + self.radius))
         if new_pos[0] != self.player.x and new_pos[1] != self.player.y:
             # can't move in both x and y direction at the same time
             if self.prio == 'x':
@@ -118,3 +139,6 @@ class Sprite(pygame.sprite.Sprite):
             math.floor(pixels[0] / self.block_size),
             math.floor(pixels[1] / self.block_size)
         )
+
+    def round_pixel(self, pixel):
+        return math.floor(pixel / self.block_size) * self.block_size
