b2133edab3a98c1d5047d8dfbbedbd177f93c79b
Emil Murman <emilmurman@hotmail.com>
2018-11-15 09:20:24 +0100

    implement #37 and make sprite names more consistent

--- client/graphics/player_sprite.py
+++ client/graphics/player_sprite.py
@@ -12,12 +12,22 @@ DIR = os.path.dirname(os.path.realpath(__file__))
 PLAYER_SPRITE_WIDTH = 16
 PLAYER_SPRITE_HEIGHT = 28
 
+# Sprite sets dict keys
+RUN_RIGHT = 'run_right'
+RUN_LEFT = 'run_left'
+IDLE_RIGHT = 'idle_right'
+IDLE_LEFT = 'idle_left'
+
 class PlayerSprite(AnimatedSprite):
     def __init__(self, player, block_size, img):
-        get_image = lambda n: os.path.join(DIR, 'sprites/sprites/' + img + '_run_anim_f' + str(n) + '.png')
-        images = []
+        def get_image(n, type_):
+            return os.path.join(DIR, 'sprites/sprites/' + img + type_ + str(n) + '.png')
+        run = []
+        idle = []
         for i in range(4):
-            images.append(get_image(i))
+            run.append(get_image(i, '_run_anim_f'))
+            idle.append(get_image(i, '_idle_anim_f'))
+        image_dict = dict([(RUN_RIGHT, run), (IDLE_RIGHT, idle)])
 
         # Scale sprites
         size_margin = 1  # make the character slightly bigger - looks nicer
@@ -28,46 +38,79 @@ class PlayerSprite(AnimatedSprite):
 
         # Sprite offsets
         x_offset = -2 * scaling_factor * size_margin    # the sprite should be moved about 2 pixels to the right
-        y_offset = 12 * scaling_factor * size_margin    # move 12 pixels up
+        y_offset = 12 * scaling_factor * size_margin    # and 12 pixels up
 
-        AnimatedSprite.__init__(self, images, (
+        AnimatedSprite.__init__(self, image_dict, (
             player.px,
             player.py,
             new_dimensions[0],
             new_dimensions[1]
         ), (x_offset, y_offset))
 
-        # Generate leftwards-facing images
-        self.images_right = self.images
-        self.images_left = []
-        for image in self.images_right:
-            self.images_left.append(pygame.transform.flip(image, True, False))
+        # Generate left-facing images
+        run_left = []
+        idle_left = []
+        for image in self.image_sets[RUN_RIGHT]:
+            run_left.append(pygame.transform.flip(image, True, False))
+        for image in self.image_sets[IDLE_RIGHT]:
+            idle_left.append(pygame.transform.flip(image, True, False))
+
+        self.image_sets[RUN_LEFT] = run_left
+        self.image_sets[IDLE_LEFT] = idle_left
 
         self.player = player
         self.block_size = block_size
 
-        self.moving_right = True
-        self.prio = 'x'
+        self.facing_right = True
+        self.moving = True
 
     def update(self, root_x, root_y):
         AnimatedSprite.update(self)
-        if self.player.vel[0] < 0 and self.moving_right:
+        if self.player.vel[0] < 0 and self.facing_right:
             self.flip_direction()
-        elif self.player.vel[0] > 0 and not self.moving_right:
+        elif self.player.vel[0] > 0 and not self.facing_right:
             self.flip_direction()
+        elif self.player.vel[0] == 0 and self.player.vel[1] == 0 and self.moving:
+            self.toggle_idle()
+        elif (self.player.vel[0] != 0 or self.player.vel[1] != 0) and not self.moving:
+            self.toggle_idle()
 
         self.x = self.player.px - root_x
         self.y = self.player.py - root_y
         self.rect[0] = math.floor(self.x - self.x_offset)
         self.rect[1] = math.floor(self.y - self.y_offset)
 
-    def flip_direction(self):
-        self.moving_right = not self.moving_right
-        self.x_offset = -self.x_offset
-        if self.images is self.images_right:
-            self.images = self.images_left
+    def toggle_idle(self):
+        self.moving = not self.moving
+        if self.current_set_key == IDLE_LEFT or self.current_set_key == IDLE_RIGHT:
+            if self.facing_right:
+                img_set = RUN_RIGHT
+            else:
+                img_set = RUN_LEFT
         else:
-            self.images = self.images_right
+            if self.facing_right:
+                img_set = IDLE_RIGHT
+            else:
+                img_set = IDLE_LEFT
+
+        self.switch_image_set(img_set)
+        self.next_image()
+
+    def flip_direction(self):
+        self.facing_right = not self.facing_right
+        self.x_offset = -self.x_offset
+        if self.current_set_key == RUN_RIGHT or self.current_set_key == IDLE_RIGHT:
+            if self.moving:
+                img_set = RUN_LEFT
+            else:
+                img_set = IDLE_LEFT
+        else:
+            if self.moving:
+                img_set = RUN_RIGHT
+            else:
+                img_set = IDLE_RIGHT
+
+        self.switch_image_set(img_set)
         self.next_image()
 
     def to_pixels(self, coords):
