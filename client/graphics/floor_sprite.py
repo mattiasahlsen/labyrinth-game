a1c2c12e2215e38a991b335701c495ef8027503e
Mattias Ahlsen <mattias.ahlsen@gmail.com>
2018-11-17 17:29:00 +0100

    Updated floor.

--- client/graphics/floor_sprite.py
+++ client/graphics/floor_sprite.py
@@ -5,15 +5,6 @@ from random import randint
 # globals
 DIR = os.path.dirname(os.path.realpath(__file__))
 
-class FloorSprite(pygame.sprite.Sprite):
-    def __init__(self, rect):
-        pygame.sprite.Sprite.__init__(self)
-
-        tile = os.path.join(DIR, 'sprites/sprites/floor_' + random_sprite() + '.png')
-
-        self.image = pygame.transform.scale(pygame.image.load(tile), (rect.w, rect.h))
-        self.rect = rect
-
 def random_sprite():
     n = 0
     r = randint(0, 100)
@@ -24,3 +15,18 @@ def random_sprite():
     else:
         n = randint(1, 8)
     return str(n)
+
+class FloorSprite(pygame.sprite.Sprite):
+    def __init__(self, rect):
+        pygame.sprite.Sprite.__init__(self)
+
+        # 80x80
+        sheet = pygame.image.load(os.path.join(DIR, 'sprites/sprites/floor_tilesheet.png'))
+
+        x = 16 * randint(1, 3)
+        y = 16 * randint(1, 3)
+        tile = sheet.subsurface(x, y, 16, 16)
+
+
+        self.image = pygame.transform.scale(tile, (rect.w, rect.h))
+        self.rect = rect
--- client/graphics/renderer.py
+++ client/graphics/renderer.py
@@ -62,7 +62,7 @@ class Renderer:
         x = round(max(0, min(math.floor(self.local_player.px - self.res / 2), self.width * self.block_size - self.res)))
         y = round(max(0, min(math.floor(self.local_player.py - self.res / 2), self.width * self.block_size - self.res)))
 
-        sub_background = self.background.subsurface(pygame.Rect(x, y, self.res, self.res))
+        sub_background = self.background.subsurface((x, y, self.res, self.res))
         self.sprites.update(x, y)
 
         self.screen.blit(sub_background, (0, 0))
